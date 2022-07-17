a27d47d9791a0e239231d7999df9b9b105256b6e
Kruk1 <albertkr@wp.pl>
2022-07-17 19:48:40 +0200

    Add responsive design

--- public/animation.js
+++ public/animation.js
@@ -1,9 +1,11 @@
 const btns = document.querySelectorAll('.therapy-btn')
 const counter = document.querySelector('.counter')
 const messageBox = document.querySelector('#text-input')
+const menuIcon = document.querySelector('.menu')
 
 btns.forEach(btn => btn.addEventListener('click', open))
 messageBox.addEventListener('input', counting)
+menuIcon.addEventListener('click', navDisplay)
 
 function open(e)
 {
@@ -19,5 +21,32 @@ function counting(e)
 {
     let maxlength = 300
     counter.textContent = maxlength - this.value.length
+    e.stopPropagation()
+}
+
+function navDisplay(e)
+{
+    if(this.children[0].getAttribute('class') === 'icon-menu')
+    {
+        this.children[0].setAttribute('class', 'icon-cancel')
+        this.nextElementSibling.classList.add('display')
+        this.nextElementSibling.classList.add('bg-nav')
+        this.nextElementSibling.classList.remove('undisplay')
+        for(let i = 0; i < this.nextElementSibling.children.length; i++)
+        {
+            this.nextElementSibling.children[i].style.animation = `display-nav ${i * 0.3}s forwards`
+        }
+    }
+    else
+    {
+        this.children[0].setAttribute('class', 'icon-menu')
+        this.nextElementSibling.classList.add('undisplay')
+        this.nextElementSibling.classList.remove('display')
+        this.nextElementSibling.classList.remove('bg-nav')
+        for(let i = 0; i < this.nextElementSibling.children.length; i++)
+        {
+            this.nextElementSibling.children[i].style.animation = ``
+        }
+    }
     e.stopPropagation()
 }
